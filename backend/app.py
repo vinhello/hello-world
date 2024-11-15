71b1be19f35e6570a4d0f30351f36ba9bb07abd8
Vincent Voong <voongvincent@gmail.com>
2024-11-14 23:57:55 -0800

    Add gunicorn to requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -19,4 +19,4 @@ def serve_react_app(path):
         return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
